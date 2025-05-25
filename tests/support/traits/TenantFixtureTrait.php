902906c6935f8a6e48566120c876cff333fdfac7
davidhirtz <david.hirtz@me.com>
2025-05-25 18:15:11 +0200

    Tenant model tests

--- tests/support/traits/TenantFixtureTrait.php
+++ tests/support/traits/TenantFixtureTrait.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace davidhirtz\yii2\tenant\tests\support\traits;
 
+use davidhirtz\yii2\tenant\models\collections\TenantCollection;
 use davidhirtz\yii2\tenant\tests\fixtures\TenantFixture;
 use davidhirtz\yii2\tenant\tests\support\UnitTester;
 
 trait TenantFixtureTrait
 {
     protected UnitTester $tester;
+
+    public function _before(): void
+    {
+        TenantCollection::invalidateCache();
+    }
 
     public function _fixtures(): array
     {
--- tests/unit/models/TenantTest.php
+++ tests/unit/models/TenantTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace davidhirtz\yii2\tenant\tests\unit\models;
 
 use Codeception\Test\Unit;
+use davidhirtz\yii2\tenant\models\collections\TenantCollection;
 use davidhirtz\yii2\tenant\models\Tenant;
 use davidhirtz\yii2\tenant\tests\support\traits\TenantFixtureTrait;
 use davidhirtz\yii2\tenant\tests\support\UnitTester;
@@ -12,14 +13,25 @@ use Yii;
 
 class TenantTest extends Unit
 {
-    protected UnitTester $tester;
     use TenantFixtureTrait;
 
-    public function testUrl(): void
+    protected UnitTester $tester;
+
+    public function testHostInfo(): void
     {
-        $tenant = Tenant::create();
-        $tenant->status = Tenant::STATUS_ENABLED;
-        $tenant->name = 'Test';
+        $tenant = $this->tester->grabTenant();
+        self::assertEquals('https://www.domain.com', $tenant->getHostInfo());
+    }
+
+    public function testPathInfo(): void
+    {
+        $tenant = $this->tester->grabTenant('path');
+        self::assertEquals('/de', $tenant->getPathInfo());
+    }
+
+    public function testUrlValidation(): void
+    {
+        $tenant = $this->createTenant();
         $tenant->url = 'www.domain.com';
         $tenant->language = 'en-US';
 
@@ -41,5 +53,74 @@ class TenantTest extends Unit
 
         self::assertTrue($tenant->save());
         self::assertEquals('https://www.new-domain.com', $tenant->url);
+        self::assertEquals(4, $tenant->position);
+    }
+
+    public function testLanguageValidation(): void
+    {
+        Yii::$app->getI18n()->setLanguages(['en-US', 'de']);
+        self::assertCount(2, Tenant::getLanguages());
+
+        $tenant = $this->createTenant();
+        $tenant->url = 'https://www.test.de';
+        $tenant->language = 'de';
+
+        self::assertTrue($tenant->save());
+        codecept_debug($tenant->getErrors());
+        self::assertEquals('de', $tenant->language);
+
+        $tenant->language = 'invalid-language';
+
+        self::assertFalse($tenant->update());
+        self::assertArrayHasKey('language', $tenant->getErrors());
+    }
+
+    public function testCookieDomainValidation(): void
+    {
+        $tenant = $this->createTenant();
+
+        $tenant->url = 'https://www.test.com';
+        $tenant->language = 'en-US';
+        $tenant->cookie_domain = 'test.com';
+
+        self::assertTrue($tenant->save());
+        self::assertEquals('test.com', $tenant->cookie_domain);
+
+        $tenant->cookie_domain = 'invalid-domain';
+
+        self::assertFalse($tenant->update());
+        self::assertArrayHasKey('cookie_domain', $tenant->getErrors());
+
+        $tenant->cookie_domain = 'https://www.test.com';
+
+        self::assertFalse($tenant->update());
+        self::assertArrayHasKey('cookie_domain', $tenant->getErrors());
+    }
+
+    public function testDelete(): void
+    {
+        $tenant = $this->tester->grabTenant('draft');
+
+        self::assertEquals(1, $tenant->delete());
+        self::assertTrue($tenant->isDeleted());
+        self::assertCount(2, TenantCollection::getAll());
+
+        $tenant = $this->tester->grabTenant();
+        self::assertEquals(1, $tenant->delete());
+
+        $default = TenantCollection::getDefault();
+        self::assertEquals(2, $default->id);
+
+        self::assertFalse($default->delete());
+        self::assertContains(Yii::t('tenant', 'TENANT_ERROR_DELETE_LAST'), $default->getErrors('id'));
+    }
+
+    protected function createTenant(): Tenant
+    {
+        $tenant = Tenant::create();
+        $tenant->status = Tenant::STATUS_ENABLED;
+        $tenant->name = 'Test';
+
+        return $tenant;
     }
 }
