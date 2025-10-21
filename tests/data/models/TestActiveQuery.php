d8b9c22f14c57d0d46c9106cc5816c207991d73d
davidhirtz <david.hirtz@me.com>
2025-10-21 15:18:38 +0200

    Version 1.3.1

--- tests/data/models/TestActiveQuery.php
+++ tests/data/models/TestActiveQuery.php
@@ -7,6 +7,9 @@ namespace davidhirtz\yii2\tenant\tests\data\models;
 use davidhirtz\yii2\skeleton\db\ActiveQuery;
 use davidhirtz\yii2\tenant\models\queries\traits\TenantQueryTrait;
 
+/**
+ * @extends ActiveQuery<TestModel>
+ */
 class TestActiveQuery extends ActiveQuery
 {
     use TenantQueryTrait;
