57e99223f7d8a6a3db80120aef0be15a04e05a8b
davidhirtz <david.hirtz@me.com>
2025-12-07 23:17:14 +0000

    Test

--- tests/unit/models/collections/TenantCollectionTest.php
+++ tests/unit/models/collections/TenantCollectionTest.php
@@ -4,12 +4,23 @@ declare(strict_types=1);
 
 namespace davidhirtz\yii2\tenant\tests\unit\models\collections;
 
-use davidhirtz\yii2\tenant\tests\base\TestCase;
+use davidhirtz\yii2\skeleton\test\TestCase;
+use davidhirtz\yii2\skeleton\web\Application;
+use davidhirtz\yii2\tenant\tests\fixtures\TenantFixture;
 use Override;
 use Yii;
 
 final class TenantCollectionTest extends TestCase
 {
+    public function fixtures(): array
+    {
+        return [
+            'tenant' => [
+                'class' => TenantFixture::class,
+            ],
+        ];
+    }
+
     #[Override]
     protected function setUp(): void
     {
@@ -17,9 +28,14 @@ final class TenantCollectionTest extends TestCase
         parent::setUp();
     }
 
+    public function getTenantFixture(): TenantFixture
+    {
+        return $this->getFixture('tenant');
+    }
+
     public function testDefault(): void
     {
-        dump(Yii::$app);
-        self::assertTrue(true);
+        echo $this->getTenantFixture()->dataFile;
+        self::assertTrue(Yii::$app instanceof Application);
     }
 }
