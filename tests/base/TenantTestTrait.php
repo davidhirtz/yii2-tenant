425f432350a333317773af0cbfbb20dafc07835d
davidhirtz <david.hirtz@me.com>
2025-12-07 22:36:28 +0000

    Trait

--- tests/base/TenantTestTrait.php
+++ tests/base/TenantTestTrait.php
@@ -6,11 +6,12 @@ namespace davidhirtz\yii2\tenant\tests\base;
 
 use Override;
 
-class TestCase extends \davidhirtz\yii2\skeleton\tests\TestCase
+trait TenantTestTrait
 {
+    #[Override]
     protected function setUp(): void
     {
-        $this->config = require __DIR__ . '/../config.php';
+        $this->config = require(__DIR__ . '/../config.php');
         parent::setUp();
     }
 }
--- tests/unit/models/collections/TenantCollectionTest.php
+++ tests/unit/models/collections/TenantCollectionTest.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace davidhirtz\yii2\tenant\tests\unit\models\collections;
 
-use davidhirtz\yii2\tenant\tests\base\TestCase;
-use Override;
+use davidhirtz\yii2\skeleton\tests\TestCase;
+use davidhirtz\yii2\tenant\tests\base\TenantTestTrait;
 use Yii;
 
 final class TenantCollectionTest extends TestCase
 {
+    use TenantTestTrait;
+
     public function testDefault(): void
     {
         dump(Yii::$app);
