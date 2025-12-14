662f830b3bffaaaba56cf36085619a8f8e688a4c
davidhirtz <david.hirtz@me.com>
2025-12-14 13:36:33 +0000

    Revert "Ignore case fix"
    This reverts commit fd6c53e1882288e76b5672387c9be45ebbf3d7bd.

--- tests/config.php
+++ tests/config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Hirtz\Tenant\Bootstrap;
 
 $basePath = (getenv('BASE_PATH') ?: getcwd());
-$config = require("$basePath/vendor/davidhirtz/yii2-skeleton/config/test.php");
+$config = require("$basePath/vendor/davidhirtz/yii2-skeleton/tests/config.php");
 
 return [
     ...$config,
