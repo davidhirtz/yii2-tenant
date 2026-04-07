bff13c9532ed0d4f59462f4c378908bd826e1419
davidhirtz <david.hirtz@me.com>
2026-04-07 17:41:19 +0200

    Added module

--- src/Modules/Admin/Module.php
+++ src/Modules/Admin/Module.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Hirtz\Tenant\Modules\Admin;
 
-use Hirtz\Skeleton\Modules\Admin\Config\MainMenuItemConfig;
 use Hirtz\Skeleton\Modules\Admin\ModuleInterface;
 use Hirtz\Tenant\Models\Tenant;
 use Hirtz\Tenant\Modules\Admin\Controllers\TenantController;
 use Override;
 use Yii;
 use yii\helpers\ArrayHelper;
+use Hirtz\Skeleton\Widgets\Navs\Nav;
+use Hirtz\Skeleton\Widgets\Navs\NavItem;
 
 /**
  * @property \Hirtz\Skeleton\Modules\Admin\Module $module
@@ -46,22 +47,14 @@ class Module extends \Hirtz\Skeleton\Base\Module implements ModuleInterface
         return Yii::t('tenant', 'TENANT_NAME_PLURAL');
     }
 
-    public function getMainMenuItems(): array
+    public function aside(Nav $nav): Nav
     {
-        return [
-            'tenants' => new MainMenuItemConfig(
-                label: $this->getName(),
-                url: $this->url,
-                icon: 'network-wired',
-                roles: [
-                    Tenant::AUTH_TENANT_CREATE,
-                    Tenant::AUTH_TENANT_UPDATE,
-                ],
-                routes: [
-                    'admin/tenant/',
-                ],
-                order: 80,
-            ),
-        ];
+        return $nav->addItem(NavItem::make()
+            ->label($this->getName())
+            ->url($this->url)
+            ->icon('network-wired')
+            ->roles([Tenant::AUTH_TENANT_CREATE, Tenant::AUTH_TENANT_UPDATE])
+            ->routes(['admin/tenant/'])
+            ->order(80));
     }
 }
