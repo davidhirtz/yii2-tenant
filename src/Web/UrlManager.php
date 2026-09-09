7747837d47b8ee1d5db203f42b8dac7b6e17ebe9
davidhirtz <david.hirtz@me.com>
2026-09-09 14:49:25 +0200

    Update UrlManager.php

--- src/Web/UrlManager.php
+++ src/Web/UrlManager.php
@@ -151,8 +151,12 @@ class UrlManager extends \Hirtz\Skeleton\Web\UrlManager
     #[Override]
     public function getDraftHostInfo(): string
     {
-        return $this->draftSubdomain
-            ? $this->replaceSubdomain($this->draftSubdomain, $this->tenant->getHostInfo())
-            : $this->tenant->getHostInfo();
+        if ($this->tenant) {
+            return $this->draftSubdomain
+                ? $this->replaceSubdomain($this->draftSubdomain, $this->tenant->getHostInfo())
+                : $this->tenant->getHostInfo();
+        }
+
+        return parent::getDraftHostInfo();
     }
 }
