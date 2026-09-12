0ec701e309b454211ced44a0657367fbe8a9aebe
davidhirtz <david.hirtz@me.com>
2026-09-12 09:06:23 +0200

    Analyse the views with PHPStan
    TenantController::actionIndex() passes a TenantActiveDataProvider, which is
    what TenantHeader::provider() expects, so the view's @var says so now.

--- resources/views/admin/tenant/index.php
+++ resources/views/admin/tenant/index.php
@@ -6,15 +6,15 @@ declare(strict_types=1);
  * @see TenantController::actionIndex()
  *
  * @var View $this
- * @var ActiveDataProvider $provider
+ * @var TenantActiveDataProvider $provider
  */
 
 use Hirtz\Skeleton\Web\View;
 use Hirtz\Skeleton\Widgets\Grids\GridContainer;
 use Hirtz\Tenant\Modules\Admin\Controllers\TenantController;
+use Hirtz\Tenant\Modules\Admin\Data\TenantActiveDataProvider;
 use Hirtz\Tenant\Modules\Admin\Widgets\Grids\TenantGridView;
 use Hirtz\Tenant\Modules\Admin\Widgets\Navs\TenantHeader;
-use yii\data\ActiveDataProvider;
 
 $this->title(Yii::t('tenant', 'TENANT_NAME_PLURAL'));
 
