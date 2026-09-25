34a0ee1f67841047fa9a3d6b60ce4b919c6bbce9
davidhirtz <david.hirtz@me.com>
2026-09-25 12:24:01 +0200

    Revert "Take the tenant permission back from managers an upgrade left it with"
    This reverts commit 72f066617927ad0dcf5a05724b5bf6eb852093a6.

--- tests/Migrations/AuthItemTest.php
+++ tests/Migrations/AuthItemTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Hirtz\Tenant\Tests\Migrations;
 
 use Hirtz\Skeleton\Models\User;
-use Hirtz\Tenant\Migrations\M260925110000ManagerTenantPermission;
 use Hirtz\Tenant\Models\Tenant;
 use Hirtz\Tenant\Test\TestCase;
 use Yii;
@@ -21,20 +20,6 @@ final class AuthItemTest extends TestCase
         $auth = Yii::$app->getAuthManager();
 
         self::assertArrayHasKey(Tenant::AUTH_TENANT, $auth->getPermissionsByRole(User::AUTH_ROLE_ADMIN));
-        self::assertArrayNotHasKey(Tenant::AUTH_TENANT, $auth->getPermissionsByRole(User::AUTH_ROLE_MANAGER));
-    }
-
-    public function testTheManagerTenantPermissionMigrationTakesTheTenantBack(): void
-    {
-        $auth = Yii::$app->getAuthManager();
-        $manager = $auth->getRole(User::AUTH_ROLE_MANAGER) ?? self::fail('No manager role.');
-        $tenant = $auth->getPermission(Tenant::AUTH_TENANT) ?? self::fail('No tenant permission.');
-
-        $auth->addChild($manager, $tenant);
-        self::assertArrayHasKey(Tenant::AUTH_TENANT, $auth->getPermissionsByRole(User::AUTH_ROLE_MANAGER));
-
-        (new M260925110000ManagerTenantPermission())->safeUp();
-
         self::assertArrayNotHasKey(Tenant::AUTH_TENANT, $auth->getPermissionsByRole(User::AUTH_ROLE_MANAGER));
     }
 }
